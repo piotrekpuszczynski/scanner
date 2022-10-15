d07973fc26fa2a03ce086c4d6c433eaa5c86d51e
piotrekpuszcz <piotrekpuszcz@icloud.com>
2022-10-15 17:34:48 +0200

    Initial Commit

---
+++ scanner/scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scannerApp.swift
+//  scanner
+//
+//  Created by Piotr Puszczyński on 15/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
